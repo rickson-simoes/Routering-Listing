2b1df67a8ba42e0ebfea82fbfa59a83842f3ca04
Rickson <rickson.simoes@hotmail.com>
2021-12-10 16:07:50 -0300

    :wrench: Programmatic Navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,20 @@
 <template>
   <p>Register for the event here</p>
+
+  <button @click="register()">Register</button>
 </template>
 
 <script>
 export default {
   props: ["event"],
+  methods: {
+    register() {
+      // this.$router.push({
+      //   name: "EventDetails",
+      // });
+
+      this.$router.replace({ name: "EventDetails" });
+    },
+  },
 };
 </script>
